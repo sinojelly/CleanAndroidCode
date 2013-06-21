15ef5235e3974dbec89420e9a295bc824a929ad7
sinojelly <sinojelly@gmail.com>
2013-06-21 20:53:20 +0800

    compile with gradle ok. and run ok.

---
+++ cleanandroidcode/src/main/java/info/piwai/cleanandroidcode/EntryPointsModule.java
@@ -0,0 +1,14 @@
+package info.piwai.cleanandroidcode;
+
+import javax.inject.Singleton;
+
+import com.squareup.otto.Bus;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module(injects={HelloAndroidActivity_.class,HelloFragment_.class})
+public class EntryPointsModule {
+
+
+}
